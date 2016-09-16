035397a03e02440748966d5a54d39d65832fc680
Joe Kutner <jpkutner@gmail.com>
2016-09-16 07:55:21 -0500

    Added testunit test

---
+++ test/compile_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testCompile() {
+  cat > ${BUILD_DIR}/Aptfile <<EOF
+s3cmd
+wget
+EOF
+
+  compile
+
+  assertCapturedSuccess
+
+  assertCaptured "Fetching .debs for s3cmd"
+  assertCaptured "Installing s3cmd_"
+  assertCaptured "Fetching .debs for wget"
+  assertCaptured "Installing wget_"
+}
